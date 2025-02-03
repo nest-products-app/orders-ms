bdecc69a3017775104dd93ebd83bda8bcd429895
David-Gcs <82048609+David-Gcs@users.noreply.github.com>
2025-02-03 18:30:27 -0400

    feat: integrate NATS for messaging and update environment configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,6 +6,7 @@ interface EnvVars {
   PORT: number;
   PRODUCTS_MICROSERVICE_HOST: string;
   PRODUCTS_MICROSERVICE_PORT: number;
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
@@ -13,10 +14,14 @@ const envsSchema = joi
     PORT: joi.number().required(),
     PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
     PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -28,4 +33,5 @@ export const envs = {
   port: envVars.PORT,
   PRODUCTS_MICROSERVICE_HOST: envVars.PRODUCTS_MICROSERVICE_HOST,
   PRODUCTS_MICROSERVICE_PORT: envVars.PRODUCTS_MICROSERVICE_PORT,
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
